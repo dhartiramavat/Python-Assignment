b59fa800fe329b27133f61b841017cc35f6be545
dhartiramavat <dhartiramavat@gmail.com>
2026-04-15 11:55:31 +0530

    Add files via upload

---
+++ A_9.py
@@ -0,0 +1,12 @@
+# Initial values
+a = 10
+b = 20
+
+print(f"Before swapping: a = {a}, b = {b}")
+
+# Using a temporary variable to swap
+temp = a
+a = b
+b = temp
+
+print(f"After swapping: a = {a}, b = {b}")
