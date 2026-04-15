f1bd72766580b7ee098f3d2c8a63feadbc23e04a
dhartiramavat <dhartiramavat@gmail.com>
2026-04-15 11:55:58 +0530

    Add files via upload

---
+++ A_10.py
@@ -0,0 +1,8 @@
+# Program to check if a number is even or odd
+
+num = int(input("Enter a whole number: "))
+
+if num % 2 == 0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
