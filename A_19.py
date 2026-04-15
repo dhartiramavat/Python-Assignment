14ae38a21f7885a923f48eef08c3e65776009243
dhartiramavat <dhartiramavat@gmail.com>
2026-04-15 11:59:13 +0530

    Add files via upload

---
+++ A_19.py
@@ -0,0 +1,6 @@
+# List of strings
+word_list = ["apple", "banana", "cherry"]
+
+# Join into one string and count 'a'
+total_count = "".join(word_list).count('a')
+print(f"Total occurrences of 'a': {total_count}") 
