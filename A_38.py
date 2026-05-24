0233dba85fa82b199f12eca5fee958746b1151df
dhartiramavat <dhartiramavat@gmail.com>
2026-05-24 17:22:09 +0530

    Add files via upload

---
+++ A_38.py
@@ -0,0 +1,6 @@
+'''Write a Python program to select an item randomly from a list.'''
+
+import random
+my_list = [10, 20, 30, 40, 50]
+random_item = random.choice(my_list)
+print("Randomly selected item:", random_item)
