d8742c8f28de18bb11e918575f2490992dd47562
dhartiramavat <dhartiramavat@gmail.com>
2026-05-24 17:20:30 +0530

    Add files via upload

---
+++ A_33.py
@@ -0,0 +1,7 @@
+'''Write a Python program to check a list is empty or not'''
+
+lst=["aba","xy","1221","ss","aaaaaa","123","america","aba","aaaaaa"]
+if len(lst)==0:
+    print("Your list is empty....")
+else:
+    print(lst)
