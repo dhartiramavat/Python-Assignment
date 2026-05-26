4d9f7121a05194688d8ca5e386ee2bb2560436d1
dhartiramavat <dhartiramavat@gmail.com>
2026-05-26 18:31:08 +0530

    Add files via upload

---
+++ A_55.py
@@ -0,0 +1,5 @@
+'''Write a Python script to merge two Python dictionaries'''
+dict1={"name":"Dharti", "age":26,"course":"data analyst"}
+dict2={"city":"Porbandar","country":"India"}
+dict1.update(dict2)
+print(dict1)
