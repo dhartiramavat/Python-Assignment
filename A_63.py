852f59116fb8d371e4c90a21fc9235b03e325112
dhartiramavat <dhartiramavat@gmail.com>
2026-05-26 18:36:39 +0530

    Add files via upload

---
+++ A_63.py
@@ -0,0 +1,9 @@
+'''Write a Python function to check whether a number is perfect or not'''
+num=int(input("Enter a to check : "))
+def checknum(a):
+    if num >=0:
+        print("Perfecto")
+    else:
+        print("Not perfecato")
+
+checknum(num)
