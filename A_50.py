8d1b08e2efc00242b28571ecb13fa3991b818394
dhartiramavat <dhartiramavat@gmail.com>
2026-05-26 18:27:04 +0530

    Add files via upload

---
+++ A_50.py
@@ -0,0 +1,8 @@
+'''Write a Python script to check if a given key already exists in a 
+dictionary..'''
+
+dict1={'a':9646, 'b':9886, 'c':8563}
+if dict1.get('b'):
+    print("Exist")
+else:
+    print("not")
