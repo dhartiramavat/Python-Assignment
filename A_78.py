4fb74a255f6f4359396ffacea28efff3a23adcaa
dhartiramavat <dhartiramavat@gmail.com>
2026-05-30 12:25:04 +0530

    Delete A_78.py

--- A_78.py
+++
@@ -1,8 +0,0 @@
-'''Write a python program to find the longest words. '''
-sentence = "I my name is sneha timra."
-words = sentence.split()
-longest = ""
-for i in words:
-    if len(i) > len(longest):
-        longest = i
-print("Longest word:", longest)
